3a7d920e31f899eb1a9d1a51706050aec3aae0f9
sodacan15 <reinwaldmarone.valeza.320401@gmail.com>
2025-08-29 17:20:10 +0800

    Update Addition.java

--- SampleMath/Addition.java
+++ SampleMath/Addition.java
@@ -5,28 +5,53 @@ public class Addition{
       Scanner sc = new Scanner(System.in);
       Add add = new Add();
 
-      System.out.print("Enter a Number: ");
-      add.checker(sc.nextLine());
+      add.a = add.checker(sc, "Enter first number: ");
+      add.b = add.checker(sc, "Enter second number: ");
+     
+      add.clearScreen();
       add.doubleValue();
+     
+      add.addNumbers();
 
   }
 }
 
 class Add {
-  int a, b;
+  int a, b, c, d; // You can have as many variables as needed
 
-  public void checker (String input) {
-     try {
-        this.a = Integer.parseInt(input);
-     } catch (Exception e) {
-        System.out.println("Invalid input bitch");
-        this.a = 0; // default value
+  public void clearScreen() {
+    try {
+      // For Windows
+      if (System.getProperty("os.name").contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      } else {
+        // For Unix/Linux/Mac
+        new ProcessBuilder("clear").inheritIO().start().waitFor();
+      }
+    } catch (Exception e) {
+      // Fallback to ANSI escape codes
+      System.out.print("\033[2J\033[H");
+      System.out.flush();
+    }
+  }
+
+  public int checker(Scanner sc, String prompt) {
+     while (true) {
+        System.out.print(prompt);
+        String input = sc.nextLine();
+        try {
+           return Integer.parseInt(input);
+        } catch (Exception e) {
+           System.out.println("Invalid input! Please enter a valid number.");
+        }
      }
   }
 
-  public void doubleValue () {
+  public void doubleValue() {
     System.out.println("Double value of a is: " + (a * 2));
   }
 
-
+  public void addNumbers() {
+    System.out.println("Sum of a and b is: " + (a + b));
+  }
 }
